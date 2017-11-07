e5373ad604ba86e3fa63733e8a15f5839b97caf1
oscarterres <oscarterres@anadia.es>
2017-11-07 20:26:23 +0100

    Nos vamos

--- src/test/java/com/anadiait/prAlmacen/PedidoTest.java
+++ src/test/java/com/anadiait/prAlmacen/PedidoTest.java
@@ -1,6 +1,12 @@
 package com.anadiait.prAlmacen;
 
 import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class PedidoTest extends TestCase {
 
